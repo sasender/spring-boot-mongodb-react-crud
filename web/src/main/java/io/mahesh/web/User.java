fdc49c50a17958966e2da33506e7075d8edca9b8
Sasendra <Sasendra@sasendra.com>
2021-08-23 13:48:04 +0530

    change

--- web/src/main/java/io/mahesh/web/User.java
+++ web/src/main/java/io/mahesh/web/User.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class User{
     @Id
     private String id;
-    private String Employ Name;
+    private String EmployName;
     private String Email;
     private String Password;
 
